6e51e623deddb696e81e9d9f29e68e1e1ec53636
JerryScript <jerryscript@gmail.com>
2012-01-25 15:56:43 -0800

    Add download status check and MD5sum check to BacksideUpdater
    Checks if download file exists
    Checks if size matches posted size in manifest
    Checks if md5sum matches posted md5sum in manifest
    
    Added some gui refinements

--- gen/com/Backside/BacksideUpdater/R.java
+++ gen/com/Backside/BacksideUpdater/R.java
@@ -11,8 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int update_checker_background=0x7f020001;
+        public static final int custom_update_dialog_icon=0x7f020000;
+        public static final int download_complete_icon=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int md5_error=0x7f020003;
+        public static final int update_checker_background=0x7f020004;
     }
     public static final class id {
         public static final int BacksideUpdaterButton=0x7f050001;
@@ -25,6 +28,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int msg_board_default_text=0x7f040003;
         public static final int update_button_text=0x7f040002;
     }
 }
--- src/com/Backside/BacksideUpdater/BacksideUpdaterActivity.java
+++ src/com/Backside/BacksideUpdater/BacksideUpdaterActivity.java
@@ -12,14 +12,17 @@ import android.app.AlertDialog;
 import android.os.*;
 import android.content.*;
 import android.net.Uri;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.content.Context;
+import android.widget.Toast;
 
 
 public class BacksideUpdaterActivity extends Activity {
 	private String updateManifestUrl = "https://raw.github.com/JerryScript/BACKside-IHO/master/README";
-	private TextView textView;
+	private static TextView textView;
 	private TextView buttonTextView;
 	private static final String BUILD_VERSION = Build.VERSION.INCREMENTAL;
 	private static final String[] SEPARATED_DATE = BUILD_VERSION.split("\\.");
@@ -28,8 +31,14 @@ public class BacksideUpdaterActivity extends Activity {
 	private String theDate;
 	private String theUrl;
 	private String theChangeLog;
+	private String romName;
+	private String theMD5;
+	private static String localFileName;
+	private static String theFileSize;
+	private static Long fileSize;
+	private static String downloadMD5;
+	private static Boolean downloadComplete;
 	private Boolean upToDate;
-    private PowerManager mPowerManager;
 
 	
 /** Called when the activity is first created. */
@@ -41,13 +50,11 @@ public class BacksideUpdaterActivity extends Activity {
 		setContentView(R.layout.main);
 
 		textView = (TextView) findViewById(R.id.pagetext);
-		textView.setText("Click to check for the lastest update");
+		textView.setText("Click to check for the lastest update\n\nBe patient after clicking!");
 		
 		buttonTextView = (TextView) findViewById(R.id.BacksideUpdaterButton);
 
-	    Activity sContext = this;
-		mPowerManager = (PowerManager) sContext.getSystemService(Context.POWER_SERVICE);
-
+		
 	}
 
 	@Override
@@ -74,52 +81,104 @@ public class BacksideUpdaterActivity extends Activity {
 					theDate = separated[0];
 					theUrl = separated[1];
 					theChangeLog = separated[2];
-					upToDate = (Integer.parseInt(theDate) >  BUILD_DATE);
-					if (upToDate) {
-						textView.setText(theChangeLog);
-						new AlertDialog.Builder(this)
-					    .setTitle("Change Log "+theDate)
-					    .setMessage(theChangeLog)
-					    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-					        public void onClick(DialogInterface dialog, int whichButton) {
-								textView.setText("A new build is available:\nBACKside-IHO-VM670-"+theDate);
-					        }
-					    }).show();
-						buttonTextView.setText("Download Now");
+					theMD5 = separated[3];
+					romName = separated[4];
+					localFileName = "/download/"+romName;
+					theFileSize = separated[5];
+					upToDate = (Integer.parseInt(theDate) <  BUILD_DATE);
+					String file = android.os.Environment.getExternalStorageDirectory().getPath() + localFileName;
+					File f = new File(file);
+					if (!upToDate) {
+						if (f.exists()) {
+							if (!checkFileSize(romName)){
+								showCustomToast("The latest build is already downloaded\n\nClick the button to check the MD5 sum");
+								textView.setText("Latest build is already downloaded.\n\nReady to check md5\n\nBe patient after clicking!");
+								buttonTextView.setText("Check MD5 Now");
+								ALREADY_CHECKED = 2;
+							} else {
+								showCustomToast("Download not yet complete\n\nCheck the notification dropdown\nfor download status.\n\nOr press back to exit the Updater,\ndelete the partially downloaded file,\nand restart Updater to try again.");
+								textView.setText("Download not complete.\n\nCheck the notification dropdown\nand try again.\n\nIf you have an incomplete download,\ndelete the partially downloaded file,\nand try again.");
+								buttonTextView.setText("Check Download Status");
+								ALREADY_CHECKED = 2;
+							}
+						} else {
+							textView.setText("Change Log "+theDate+"\n\n"+theChangeLog);
+							showCustomToast("A new build is available:\n\nBACKside-IHO-VM670-"+theDate);
+							buttonTextView.setText("Download Now");
+						}
 					} else {
 						textView.setText("Current: "+BUILD_DATE+"\n\nAvailable: "+theDate+"\n\nCheck again later");
 						buttonTextView.setText("Already Up To Date");
 					}
 				} else {
 					if (ALREADY_CHECKED == 1){
-						if(!upToDate){
+						if(upToDate){
 							System.exit(0);
 						} else {
 							ALREADY_CHECKED = 2;
 							Intent downloadUpdate = new Intent(Intent.ACTION_VIEW);
 							downloadUpdate.setData(Uri.parse(theUrl));
 							startActivity(downloadUpdate);
-							textView.setText("Wait for download to complete\nReboot into recovery\nWipe cache & dalvik cache\nThen flash the zip file");
-							buttonTextView.setText("Reboot into Recovery Now");
+							textView.setText("Wait for download to complete\n\nReboot into recovery\n\nWipe cache & dalvik cache\nThen flash the zip file");
+							buttonTextView.setText("Check Download Status");
 						}
 					} else {
-						new AlertDialog.Builder(this)
-					    .setTitle("Wait for download to complete!")
-					    .setMessage("Check the notification dropdown!\n\nOnce the download completes,\nyou can reboot into recovery.\nWipe cache & dalvik,\nThen flash the zip file")
-					    .setPositiveButton("Reboot Recovery", new DialogInterface.OnClickListener() {
-					        public void onClick(DialogInterface dialog, int whichButton) {
-								textView.setText("Rebooting into Recovery...");
-								RebootCmd("reboot", "recovery", mPowerManager);
-					        }
-					    })
-					    .setNegativeButton("Later", new DialogInterface.OnClickListener() {
-					        public void onClick(DialogInterface dialog, int whichButton) {
-								textView.setText("Yummy Gingerbread!");
-								System.exit(0);
-					        }
-					    }).show();
+						downloadComplete = checkFileSize(romName);
+						if (!(downloadComplete)) {
+							if(checkMD5(theMD5, romName)){
+								new AlertDialog.Builder(this)
+								.setTitle("MD5 Verified!")
+								.setIcon(R.drawable.download_complete_icon)
+								.setMessage("Reboot into recovery,\nwipe cache & dalvik-cache,\nthen flash the zip file located\nin the download directory")
+								.setPositiveButton("Reboot Recovery", new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog, int whichButton) {
+										textView.setText("Rebooting into Recovery...");
+										RebootCmd("reboot", "recovery");
+									}
+								})
+								.setNegativeButton("Later", new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog, int whichButton) {
+										textView.setText("Yummy Gingerbread!");
+										System.exit(0);
+										}
+								}).show();
+							} else {
+								new AlertDialog.Builder(this)
+								.setTitle("Download Error")
+								.setIcon(R.drawable.md5_error)
+								.setMessage("The downloaded file md5\n"+downloadMD5+"\ndoes not match the build\n"+theMD5+"\nTry downloading again!")
+								.setPositiveButton("Download again", new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog, int whichButton) {
+										ALREADY_CHECKED = 2;
+										textView.setText("Wait for download to complete\n\nCheck the notification dropdown");
+										buttonTextView.setText("Check Again");
+										Intent downloadUpdate = new Intent(Intent.ACTION_VIEW);
+										downloadUpdate.setData(Uri.parse(theUrl));
+										startActivity(downloadUpdate);
+										}
+								})
+								.setNegativeButton("Later", new DialogInterface.OnClickListener() {
+										public void onClick(DialogInterface dialog, int whichButton) {
+										textView.setText("Yummy Gingerbread!");
+										System.exit(0);
+										}
+								}).show();
+							}
+						} else {
+							ALREADY_CHECKED = 2;
+							String file = android.os.Environment.getExternalStorageDirectory().getPath() + localFileName;
+							File f = new File(file);
+							if (f.exists()) {
+								showCustomToast("Download not yet complete\n\nCheck the notification dropdown\nfor download status.\n\nOr press back to exit the Updater,\ndelete the partially downloaded file,\nand restart Updater to try again.");
+							} else {
+								ALREADY_CHECKED = 1;
+								showCustomToast("Download has not started\n\nClick the button to try again now.");
+								buttonTextView.setText("Download Now");
+							}
+						}
 					}
 				}
+				
 			}
 
 			catch (Exception e) {
@@ -130,11 +189,11 @@ public class BacksideUpdaterActivity extends Activity {
 			}
 		}
 
-	public void RebootCmd(String cmd, String args, PowerManager powermanager) {
+	public void RebootCmd(String cmd, String args) {
 
 		new AlertDialog.Builder(this)
 	    .setTitle("Reboot into Recovery")
-	    .setMessage("Are you sure you want\nto reboot into recovery now?")
+	    .setMessage("Are you sure you want to\nreboot into recovery now?")
 	    .setPositiveButton("Reboot Recovery", new DialogInterface.OnClickListener() {
 	        public void onClick(DialogInterface dialog, int whichButton) {
 				textView.setText("Rebooting into Recovery...");
@@ -152,6 +211,60 @@ public class BacksideUpdaterActivity extends Activity {
 				System.exit(0);
 	        }
 	    }).show();
+	}
+	
+	public void showCustomToast(String str) {
+		Toast toast = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG);
+		toast.setGravity(Gravity.CENTER, 0, 0);
+		LinearLayout toastView = (LinearLayout) toast.getView();
+		ImageView imageCodeProject = new ImageView(getApplicationContext());
+		imageCodeProject.setImageResource(R.drawable.custom_update_dialog_icon);
+		toastView.addView(imageCodeProject, 0);
+		toast.show();
+
+	}
+	
+	public static boolean checkFileSize(String fileName) {
+		try {
+		File file = new File("/sdcard"+localFileName);
+		
+		fileSize = file.length() / 1024 / 1024;
+		
+		return (fileSize < Long.valueOf(theFileSize));
+		} catch (Exception e) {
+			return true;
+		}
+		
+		
+	}
+	
+	public static boolean checkMD5(String md5, String fileName) throws IOException {
+		textView.setText("Checking the md5sum...");
+
+		if (md5 == null || md5 == "" || fileName == null) {
+			return false;
+			}
+		
+		String calculatedDigest = calculateMD5("/sdcard"+localFileName);
+		
+		if (calculatedDigest == null) {
+			return false;
+			}
+		
+		return calculatedDigest.equalsIgnoreCase(md5);
+		
+	}
+	
+	public static String calculateMD5(String fileName) throws IOException {
+		java.lang.Process process = Runtime.getRuntime().exec("md5sum "+fileName);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+		process.getInputStream()));
+		
+		String[] results =  bufferedReader.readLine().split(" ");
+		downloadMD5 = results[0];
+		
+		return downloadMD5;
+	
 	}
 	
 }
